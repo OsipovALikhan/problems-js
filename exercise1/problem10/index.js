208a1b2efe946f5c40b81c8e0f493fcd5bbd877a
Osipov Alikhan <98189449+OsipovALikhan@users.noreply.github.com>
2023-08-30 00:36:24 +0600

    solving problem 10

--- exercise1/problem10/index.js
+++ exercise1/problem10/index.js
@@ -1,5 +1,15 @@
 function move() {
-  // Your code
+  let result = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] >= 'a' && str[i] <= 'z') {
+      result += String.fromCharCode(str.charCodeAt(i) + 1);
+    } else if (str[i] >= 'A' && str[i] <= 'Z') {
+      result += String.fromCharCode(str.charCodeAt(i) + 1);
+    } else {
+      result += str[i];
+    }
+  }
+  return result;
 }
 
 module.exports = move;
